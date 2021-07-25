49c5981db6e07750f6c6cd912ea26fb0f0f1f5ff
root <root@ip-172-31-18-167.us-west-2.compute.internal>
2021-07-25 17:38:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/app1',(req,res)=>
+{
+    res.send("From /app1 path i am "+process.env.HOSTNAME)
+})
+
+app.get('/app2',(req,res)=>
+{
+    res.send("From /app2 path i am "+process.env.HOSTNAME)
+})
+
+
+app.get('/app3',(req,res)=>
+{
+    res.send("From /app3 path i am "+process.env.HOSTNAME)
+})
+
+
+app.get('/app4',(req,res)=>
+{
+    res.send("From /app4 path i am "+process.env.HOSTNAME)
+})
+
+
+
+
+app.listen(8080,() =>
+{
+    console.log("The container has started listening on port 8080")
+ 
+  
+})
