ddfe6db985315b31246ae0a2e8bf9ee819547693
Saroj <78268712+enkirus@users.noreply.github.com>
2021-08-02 22:38:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const app = express()
 app.get('/app1',(req,res)=>
 {
    // res.send("From /app1 path i am "+process.env.hostname)
-    res.send("From /app1 path ")
+    res.send("From /app1 path instance1 ")
 })
 
 app.get('/app2',(req,res)=>
 {
-    res.send("From /app2 path ")
+    res.send("From /app2 path instance1 ")
 })
 
 
