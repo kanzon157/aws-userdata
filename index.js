7b91ceac72b85b9080fde8cd527678c60b67c12c
Saroj <78268712+enkirus@users.noreply.github.com>
2021-08-02 12:09:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,23 +14,12 @@ app.get('/app2',(req,res)=>
 })
 
 
-app.get('/app3',(req,res)=>
+
+
+
+app.listen(8050,() =>
 {
-    res.send("From /app3 path i am "+process.env.hostname)
-})
-
-
-app.get('/app4',(req,res)=>
-{
-    res.send("From /app4 path i am "+process.env.hostname)
-})
-
-
-
-
-app.listen(8080,() =>
-{
-    console.log("The container has started listening on port 8080")
+    console.log("The container has started listening on port 8050")
  
   
 })
