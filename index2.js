f8cbbbee93d773c1312774cc15d85c86cb6f5a7b
Saroj <78268712+enkirus@users.noreply.github.com>
2021-08-02 12:10:00 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,28 @@
 
+const express = require('express')
+const app = express()
+
+
+
+
+
+app.get('/app3',(req,res)=>
+{
+    res.send("From /app3 path i am "+process.env.hostname)
+})
+
+
+app.get('/app4',(req,res)=>
+{
+    res.send("From /app4 path i am "+process.env.hostname)
+})
+
+
+
+
+app.listen(8050,() =>
+{
+    console.log("The container has started listening on port 8050")
+ 
+  
+})
